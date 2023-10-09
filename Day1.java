ff00489121c0bd00de1f4a15d610d1c97c223059
Tasyawahdini <99948842+Tasyawahdini@users.noreply.github.com>
2023-10-09 19:14:20 +0700

    Day1.tipedata

---
+++ Day1.java
@@ -0,0 +1,27 @@
+
+package day1;
+
+public class Day1 {
+
+    public static void main(String[] args) {
+        String nama = "Tasya wahdini";
+        String alamat = "Malunda";
+        int usia = 20;
+        double tinggi_badan = 155;
+        float berat_badan = 40;
+        String Status = "Mahasiswa";
+        String hobi = "Memasak";
+
+        // Menampilkan informasi perkenalan ke layar
+        System.out.println("Halo, perkenalkan!");
+        System.out.println("Nama saya " + nama + ".");
+        System.out.println("Saya dari" + alamat + ".");
+        System.out.println("Saya berusia " + usia + " tahun.");
+        System.out.println("tinggi badan saya" + tinggi_badan + ".");
+        System.out.println("berat badan saya" + berat_badan + ".");
+        System.out.println("Status saya sebagai " + Status + ".");
+        System.out.println("Hobi saya adalah " + hobi + ".");
+        }
+    }
+  
+
