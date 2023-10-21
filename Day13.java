223ea0bde2cfd6d9120ca39733c7e336a58ea960
Tasyawahdini <99948842+Tasyawahdini@users.noreply.github.com>
2023-10-21 18:49:36 +0800

    perulangan

---
+++ Day13.java
@@ -0,0 +1,15 @@
+package day13;
+
+public class Day13 {
+
+    public static void main(String[] args) {
+       
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+            
+        
+ 
