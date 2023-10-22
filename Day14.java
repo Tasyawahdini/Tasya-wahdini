60e282fe85375d28f26be9512c0bf5fe307dbb10
Tasyawahdini <99948842+Tasyawahdini@users.noreply.github.com>
2023-10-22 21:59:14 +0800

    Add files via upload

---
+++ Day14.java
@@ -0,0 +1,18 @@
+package day14;
+
+public class Day14 {
+
+    public static void main(String[] args) {
+        String nama = "Tasya wahdini";
+        String alamat = "Malunda";
+        int umur = 20;
+        String status = "Mahasiswa";
+        
+        System.out.println("Biodata singkat");
+        System.out.println("Nama saya   : " + nama );
+        System.out.println("Alamat      : " + alamat);
+        System.out.println("Umur        : " + umur);
+        System.out.println("Status      : " + status);
+    }
+    
+}
