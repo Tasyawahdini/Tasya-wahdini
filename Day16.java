959ef8b6c83407f583706457a6a213b5505f2219
Tasyawahdini <99948842+Tasyawahdini@users.noreply.github.com>
2023-10-24 21:19:25 +0800

    Operator aritmatika

---
+++ Day16.java
@@ -0,0 +1,23 @@
+package day16;
+
+public class Day16 {
+
+    public static void main(String[] args) {
+       int nilai1 = 100;
+       int nilai2 = 20;
+       
+       int penjumlahan = nilai1 + nilai2;
+       int pengurangan = nilai1 - nilai2;
+       int perkalian = nilai1 * nilai2;
+       int bagi = nilai1 / nilai2;
+       int sisaBagi = nilai1 % nilai2;
+       
+        System.out.println("Hasil penjumlahan   "     + penjumlahan);
+        System.out.println("Hasil pengurangan   "     + pengurangan);
+        System.out.println("Hasil perkalian     "     + perkalian);
+        System.out.println("Hasil pembagian     "     + bagi);
+        System.out.println("Hasil dari sisa bagi"     + sisaBagi);
+       
+    }
+    
+}
