900028bae160118dfd13d8eb3a2980644bf4354f
Tasyawahdini <99948842+Tasyawahdini@users.noreply.github.com>
2023-10-26 16:46:21 +0800

    Perulangan while

---
+++ Day18.java
@@ -0,0 +1,13 @@
+package day18;
+
+public class Day18 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <=1000){
+            System.out.println(i + " Hari ini sudah hari ke 18 ");
+            i++;
+        }
+    
+    }
+}
