d2091f040e14ee3d94898ee62e52ff751123d78a
Tasyawahdini <99948842+Tasyawahdini@users.noreply.github.com>
2023-10-27 15:24:58 +0800

    Perulangan Do-while

---
+++ Day19.java
@@ -0,0 +1,15 @@
+package day19;
+
+public class Day19 {
+
+    public static void main(String[] args) {
+       int perulangan = 0;
+
+        do {
+            System.out.println(perulangan + "Hari ke 19 saya membahas tentang Perulangan Do-while");
+            perulangan++;
+        } while (perulangan <500 );
+    }
+}
+
+    
