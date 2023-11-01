8811f8c300ba2465f6e9c53239f9bc0aa74a3004
Tasyawahdini <99948842+Tasyawahdini@users.noreply.github.com>
2023-11-01 16:21:36 +0800

    membuat sebuah inputan sederhana

---
+++ Day24.java
@@ -0,0 +1,18 @@
+package day24;
+
+import java.util.Scanner;
+
+public class Day24 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nama;
+        
+        System.out.print("Masukkan nama anda : ");
+        nama = sc.nextLine();
+        
+        System.out.println("Nama saya adalah " + nama);
+        
+    }
+    
+}
