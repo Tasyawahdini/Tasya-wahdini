2b99bfec3fcee40868e3051854db735dfc16b1c5
Tasyawahdini <99948842+Tasyawahdini@users.noreply.github.com>
2023-11-09 20:24:26 +0800

    Aritmatika

---
+++ Day32.java
@@ -0,0 +1,26 @@
+package day32;
+
+public class Day32 {
+
+    public static void main(String[] args) {
+        int nilai1 = 25;
+        int nilai2 = 12;
+        
+        int penjumlahan = nilai1 + nilai2;
+        int pengurangan = nilai1 - nilai2;
+        int perkalian = nilai1 * nilai2;
+        int pembagian = nilai1 / nilai2;
+        int sisaBagi = nilai1 % nilai2;
+        
+        System.out.println("Nilai 1 : " + nilai1);
+        System.out.println("Nilai 2 : " + nilai2);
+        System.out.println("-----------------------------");
+        System.out.println("        Hasil       ");
+        System.out.println("Penjumlaha  = " + penjumlahan);
+        System.out.println("Pengurangan = " + pengurangan);
+        System.out.println("Perkalian   = " + perkalian);
+        System.out.println("Pembagian   = " + pembagian);
+        System.out.println("Sisa bagi   = " + sisaBagi);
+    }
+    
+}
